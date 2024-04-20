19e18c942b60a75c27d7cee444d4f936177df06d
shobitkhatri05 <shobitkhatri62@gmail.com>
2024-04-20 23:37:15 +0530

    image-capture

---
+++ Image-Capture/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ImageCapture from "./components/imageCapture";
+
+// eslint-disable-next-line no-unused-vars
+
+function App() {
+  return (
+    <>
+      <ImageCapture />
+    </>
+  );
+}
+
+export default App;
